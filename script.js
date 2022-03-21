ada6448131cdbc5f53c02c6427a62e30ba7ede6d
Maxim Zmeyok <maxim_zmeyok@mail.ru>
2022-03-21 11:57:19 +0300

    feat: made an ability to create a task and add it to web-page

--- script.js
+++ script.js
@@ -1,26 +1,48 @@
+let tasksObj = [];
+
 const addTaskButton = document.querySelector('#addTaskButton');
 addTaskButton.addEventListener('click', makePopCreate);
 
+class Task {
+  constructor(taskName, expirationDate) {
+    this.taskName = taskName;
+    this.expirationDate = expirationDate;
+  }
+  getInnerHtml() {
+    return `
+    <div>
+      <p>Task: <span>${this.taskName}</span></p>
+      <p>Expiration date: <span>${this.expirationDate}</span></p>
+    </div>
+    `;
+  }
+}
+
 function makePopCreate() {
-  let popCreate = document.createElement("section");
-  popCreate.classList.add("pop");
-  popCreate.innerHTML = `
+  let popCreated = document.createElement("section");
+  popCreated.classList.add("pop");
+  popCreated.innerHTML = `
   <div class="pop-container">
     <h2>Create a new task</h2>
-    <input placeholder="Enter a task name" type="text" required>
-    <p>Creation date:</p>
-    <input type="date" required>
+    <input placeholder="Enter a task name" type="text" id="taskName" value="Make a task" required>
     <p>Expiration date:</p>
-    <input type="date" required>
+    <input type="date" id="expirationDate" value="2022-03-21" required>
     <button class="button" id="addTaskButtonOk">Ok</button>
     <button class="button" id="addTaskButtonCancel">Cancel</button>
   </div>
   `;
-  document.body.append(popCreate);
+  document.body.append(popCreated);
   document.querySelector('#addTaskButtonOk').addEventListener('click', function() {
-    popCreate.remove();
+    let taskName = document.querySelector('#taskName').value;
+    let expirationDate = document.querySelector('#expirationDate').value;
+    if (taskName && expirationDate) {
+      let task = new Task(taskName, expirationDate);
+      tasksObj.push(task);
+      document.querySelector('#tasks').innerHTML += task.getInnerHtml();
+      popCreated.remove();
+    }
   });
   document.querySelector('#addTaskButtonCancel').addEventListener('click', function() {
-    popCreate.remove();
+    popCreated.remove();
   });
 }
