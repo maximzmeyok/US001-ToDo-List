bd9960f5f9dabfe96188d89e61d34e91ea359dcd
Maxim Zmeyok <maxim_zmeyok@mail.ru>
2022-03-23 11:52:08 +0300

    refactor: devided code into several files, added class for Pop

--- Task.js
+++ Task.js
@@ -4,35 +4,35 @@ export class Task {
   constructor(taskName, expirationDate) {
     this.taskName = taskName;
     this.expirationDate = expirationDate;
-    this.isCompleted = false;
     this.id = `${Date.now()}`;
+    this.isCompleted = false;
   }
 
-  getInnerHtml() {
+  createHtml() {
     return `
     <div id="${this.id}">
-      <div>
-      <p>Task: <span>${this.taskName}</span></p>
-      <p>Expiration date: <span>${this.expirationDate}</span></p>
-      </div>
-      <div>
-      <input type="checkbox" class="checkbox" id="${this.id}">
-      <p class="gear" id="${this.id}">&#9881;</p>
-      <p class="cross" id="${this.id}">&#9746;</p>
-      </div>
+    <div>
+    <p>Task: <span>${this.taskName}</span></p>
+    <p>Expiration date: <span>${this.expirationDate}</span></p>
+    </div>
+    <div>
+    <input type="checkbox" class="checkbox" id="${this.id}">
+    <p class="gear" id="${this.id}">&#9881;</p>
+    <p class="cross" id="${this.id}">&#9746;</p>
+    </div>
     </div>
     `;
   }
 
   static removeTask(taskId) {
     document.getElementById(taskId).remove();
-    const removedElementIndex = tasksArray.findIndex(item => item.id === taskId);
-    tasksArray.splice(removedElementIndex, 1);
+    const removedTaskIndex = tasksArray.findIndex(item => item.id === taskId);
+    tasksArray.splice(removedTaskIndex, 1);
   }
 
   static completeTask(taskId) {
     document.getElementById(taskId).classList.toggle('line-through');
-    const changedElementIndex = tasksArray.findIndex(item => item.id === taskId);
-    tasksArray[changedElementIndex].isCompleted = !tasksArray[changedElementIndex].isCompleted;
+    const changedTaskIndex = tasksArray.findIndex(item => item.id === taskId);
+    tasksArray[changedTaskIndex].isCompleted = !tasksArray[changedTaskIndex].isCompleted;
   }
 }
--- script.js
+++ script.js
@@ -1,79 +1,23 @@
 import {Task} from "./Task.js";
+import {Pop} from "./Pop.js";
 
 export const tasksArray = [];
 
 const tasksArea = document.querySelector('#tasks');
 const addTaskButton = document.querySelector('#addTaskButton');
 
-addTaskButton.addEventListener('click', makePopCreate);
+addTaskButton.addEventListener('click', function(event) {
+  Pop.createPop();
+})
+
 tasksArea.addEventListener('click', function(event) {
-  let target = event.target;
+  const target = event.target;
 
   if (target.classList.contains('cross')) {
     Task.removeTask(target.id);
   } else if (target.classList.contains('checkbox')) {
     Task.completeTask(target.id);
   } else if (target.classList.contains('gear')) {
-    makePopChange(target.id);
+    Pop.createPop(target.id);
   }
 })
-
-function makePopCreate() {
-  let popCreated = document.createElement("section");
-  popCreated.classList.add("pop");
-  popCreated.innerHTML = `
-  <div class="pop-container">
-    <h2>Create a new task</h2>
-    <input placeholder="Enter a task name" type="text" id="taskName" required value="111111111">
-    <p>Expiration date:</p>
-    <input type="date" id="expirationDate" required value="2022-03-22">
-    <button class="button" id="addTaskButtonOk">Ok</button>
-    <button class="button" id="addTaskButtonCancel">Cancel</button>
-  </div>
-  `;
-  document.body.append(popCreated);
-  document.querySelector('#addTaskButtonOk').addEventListener('click', function() {
-    let taskName = document.querySelector('#taskName').value;
-    let expirationDate = document.querySelector('#expirationDate').value;
-    if (taskName && expirationDate) {
-      let task = new Task(taskName, expirationDate);
-      tasksArray.push(task);
-      document.querySelector('#tasks').innerHTML += task.getInnerHtml();
-      popCreated.remove();
-    }
-  });
-  document.querySelector('#addTaskButtonCancel').addEventListener('click', function() {
-    popCreated.remove();
-  });
-}
-
-function makePopChange(id) {
-  let popChange = document.createElement("section");
-  popChange.classList.add("pop");
-  let elem = tasksArray.find(item => item.id === id);
-  popChange.innerHTML = ` 
-  <div class="pop-container">
-    <h2>Change a task</h2>
-    <input placeholder="Enter a task name" type="text" id="taskName" value="${elem.taskName}" required>
-    <p>Expiration date:</p>
-    <input type="date" id="expirationDate" value="${elem.expirationDate}" required>
-    <button class="button" id="addTaskButtonSave">Save</button>
-    <button class="button" id="addTaskButtonCancel">Cancel</button>
-  </div>
-  `;
-  document.body.append(popChange);
-  document.querySelector('#addTaskButtonSave').addEventListener('click', function() {
-    let taskName = document.querySelector('#taskName').value;
-    let expirationDate = document.querySelector('#expirationDate').value;
-    if (taskName && expirationDate) {
-      elem.taskName = taskName;
-      elem.expirationDate = expirationDate;
-      document.getElementById(`${id}`).firstElementChild.firstElementChild.firstElementChild.innerHTML = `${taskName}`;
-      document.getElementById(`${id}`).firstElementChild.lastElementChild.firstElementChild.innerHTML = `${expirationDate}`;
-      popChange.remove();
-    }
-  });
-  document.querySelector('#addTaskButtonCancel').addEventListener('click', function() {
-    popChange.remove();
-  });
-}
